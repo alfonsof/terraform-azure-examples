fda0d4e140696b444939e505b8bc956f80a05ff5
alfonsof <alfonsofb@gmail.com>
2018-08-10 16:28:07 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Terraform examples on Microsoft Azure
+
+Terraform is used to create, manage, and update infrastructure resources such as physical machines, VMs, network switches, containers, and more. Almost any infrastructure type can be represented as a resource in Terraform.
+
+This repo contains [Terraform](https://www.terraform.io/) code examples on Microsoft Azure.
+
+The Github repository
+[https://github.com/alfonsof/terraform-examples-aws](https://github.com/alfonsof/terraform-examples-aws)
+contains the code samples based in the book *[Terraform: Up and Running](http://www.terraformupandrunning.com)* by [Yevgeniy Brikman](http://www.ybrikman.com). But those examples use AWS (Amazon Web Services).
+
+Terraform also supports other Cloud providers and this Github repository contains the code samples of the book on Microsoft Azure.
+
+## Quick start
+
+All the code is in the [code](/code) folder.
+
+The code consists of Terraform examples using HashiCorp Configuration Language (HCL) on Microsoft Azure.
+
+For instructions on running the code, please consult the README in each folder.
+
+## License
+
+This code is released under the MIT License. See LICENSE file.
---
+++ code/01-hello-world/main.tf
@@ -0,0 +1,70 @@
+provider "azurerm" {
+}
+ 
+# Create Resource Group if it doesn’t exist
+resource "azurerm_resource_group" "tfexample" {
+  name     = "myTerraformResourceGroup"
+  location = "West Europe"
+}
+
+# Create Virtual Network
+resource "azurerm_virtual_network" "tfexample" {
+  name                = "myTerraformVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.tfexample.location}"
+  resource_group_name = "${azurerm_resource_group.tfexample.name}"
+}
+
+# Create Subnet in the Virtual Network
+resource "azurerm_subnet" "tfexample" {
+  name                 = "myTerraformSubnet"
+  resource_group_name  = "${azurerm_resource_group.tfexample.name}"
+  virtual_network_name = "${azurerm_virtual_network.tfexample.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+# Create Network Interface
+resource "azurerm_network_interface" "tfexample" {
+  name                = "myTerraformNic"
+  location            = "${azurerm_resource_group.tfexample.location}"
+  resource_group_name = "${azurerm_resource_group.tfexample.name}"
+
+  ip_configuration {
+    name                          = "myTerraformNicConfiguration"
+    subnet_id                     = "${azurerm_subnet.tfexample.id}"
+    private_ip_address_allocation = "dynamic"
+  }
+}
+
+# Create Virtual Machine
+resource "azurerm_virtual_machine" "tfexample" {
+  name                  = "myTerraformVM"
+  location              = "${azurerm_resource_group.tfexample.location}"
+  resource_group_name   = "${azurerm_resource_group.tfexample.name}"
+  network_interface_ids = ["${azurerm_network_interface.tfexample.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myOsDisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
